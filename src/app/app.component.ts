a5425bff6ed3a7a5d69e719790a629304876e4f1
Canol <canoldevelopment@gmail.com>
2024-10-27 19:18:05 +0100

    Updated appComponent and html with styling and app-inventory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InventoryComponent } from "./components/inventory/inventory.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, InventoryComponent]
+  imports: [RouterOutlet, InventoryComponent, MatToolbarModule]
 })
 export class AppComponent {
   title = 'Gilded Tros';
