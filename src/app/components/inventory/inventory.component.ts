cf1c2e18f95f7b1521ec0e9a75000c0b0c1f05bd
Canol <canoldevelopment@gmail.com>
2024-10-27 19:28:08 +0100

    Updated inventory component with getting items

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Item } from '../../interfaces/item.interface';
+import { ItemApiService } from '../../services/item/item-api.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
-export class InventoryComponent {
+export class InventoryComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  items = signal<Item[]>([]);
 
+  constructor(
+    private itemApiService: ItemApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems(): void {
+    this.itemApiService.getItems()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((items: Item[]) => {
+        this.items.set(items);
+      });
+  }
 }
