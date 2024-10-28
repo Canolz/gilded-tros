47aa0962a5646d7ea7cb36ae6419172bd51f2872
Canol <canoldevelopment@gmail.com>
2024-10-28 21:51:21 +0100

    Updated inventoryComponent with added api call, nextDay feature and styling

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -6,20 +6,27 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ItemComponent } from '../item/item.component';
+import { InventoryService } from '../../services/inventory/inventory.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css',
-  imports: [MatGridListModule, MatCardModule, MatButtonModule, ItemComponent],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    ItemComponent]
 })
 export class InventoryComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   items: Item[] = [];
 
   constructor(
-    private itemApiService: ItemApiService
+    private itemApiService: ItemApiService,
+    private inventoryService: InventoryService
   ) { }
 
   ngOnInit(): void {
@@ -27,10 +34,18 @@ export class InventoryComponent implements OnInit {
   }
 
   loadItems(): void {
-    this.itemApiService.getItems()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((items: Item[]) => {
-        this.items = items;
-      });
+    forkJoin([
+      this.itemApiService.getItems(),
+      this.itemApiService.getSmellyItems()
+    ])
+    .pipe(takeUntilDestroyed(this.destroyRef))
+    .subscribe(([normalItems, smellyItems]: [Item[], Item[]]) => {
+      this.items = [...normalItems, ...smellyItems];
+      this.inventoryService.updateQuality(this.items);
+    });
+  }
+
+  nextDay(): void {
+    this.inventoryService.updateQuality(this.items);
   }
 }
