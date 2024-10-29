8c772d2673ef6fed4d4a2d3625147a077f74c995
Canol <canoldevelopment@gmail.com>
2024-10-29 17:36:34 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { InventoryComponent } from "./components/inventory/inventory.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -8,7 +7,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, InventoryComponent, MatToolbarModule]
+  imports: [InventoryComponent, MatToolbarModule]
 })
 export class AppComponent {
   title = 'Gilded Tros';
--- src/app/models/item.model.ts
+++
@@ -1,9 +0,0 @@
-import { Item } from '../interfaces/item.interface';
-
-export class ItemModel implements Item {
-    constructor(public name: string, public sellIn: number, public quality: number) { }
-
-    public toString(): string {
-        return `${this.name}, ${this.sellIn}, ${this.quality}`;
-    }
-}
